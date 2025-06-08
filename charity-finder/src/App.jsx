261a7e12c9d811e9d67052875417f35a41f1b1a8
lora <3526039967@qq.com>
2025-06-08 16:30:31 +0800

    add top struct

---
+++ charity-finder/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import Top  from './top/top'
+
+function App() {
+return(
+  <Top/>
+)
+}
+
+export default App
---
+++ charity-finder/src/top/top.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Top= ()=>{
+    return(
+        <header className='bg-gradient-to-r from-blue-600 to-purple-500 text-center text-white py-12 '>
+            <h1 className='text-4xl font-bold mb-4'>慈善组织搜索平台</h1>
+            <p className=' max-w-2xl mx-auto text-lg'>
+                通过搜索关键词，快速找到最接近的慈善组织。
+            </p>
+        </header>
+    )
+
+}
+export default Top;
